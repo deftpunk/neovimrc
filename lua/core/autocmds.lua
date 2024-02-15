ba3c43006e87e34f1ffb4952188ac38f48b4c514
Erick Bodine <erick.bodine@gmail.com>
2024-02-15 14:03:32 -0700

    adding remove trailing whitespace autocmd

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -61,3 +61,13 @@ autocmd('BufLeave', {
   pattern = 'term://*',
   command = 'stopinsert'
 })
+
+-- Remove trailing whitespace.
+autocmd({ "BufWritePre" }, {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
+})
