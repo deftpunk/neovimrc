3511f8d87652a9eddc39741f8dffe3329001e961
Erick Bodine <erick.bodine@gmail.com>
2024-12-09 11:44:00 -0700

    some option work.
    - turn off eob fillchar, was causing an issue??
    - use JetBrainsMono font
    - turn off some language providers that i don't care about

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -12,6 +12,10 @@ vim.o.foldlevelstart = 99
 -- Setting to true will close all folds.
 -- vim.o.foldenable = true
 
+-- the last windwow will show the statusline if there are at least two windows.
+vim.o.laststatus = 1
+-- FIX: this is causing an issue for some reason
+-- vim.o.fillchars:append { eob = "~" }
 
 vim.o.termguicolors = true
 vim.o.syntax = 'on'
@@ -51,16 +55,23 @@ vim.wo.signcolumn = 'yes'
 vim.wo.wrap = false
 
 if vim.g.neovide then
-  vim.o.guifont="MesloLGM Nerd Font:h10"
+  -- this one almost makes the gap in indent-blankline.nvim characters go away.
+  -- the gap is function of the font and the OS window manager; most terminals
+  -- automatically patch the fonts to make it look better.
+  -- https://github.com/lukas-reineke/indent-blankline.nvim/issues/422
+  vim.o.guifont="JetBrainsMono Nerd Font:h12"
   vim.g.neovide_remember_window_size = true
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.print(vim.g.neovide_version)
 else
-  -- vim.opt.guifont='RobotoMono_Nerd_Font:h14:b:i:u:s'
   vim.opt.guifont='MesloLGM_Nerd_Font_Mono:h14:b:i:u:s'
 end
 
+-- disable providers that we don't need/use.
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 -- Show colored columns so I know how far I've typed without looking down at the
 -- statusline all the time.
