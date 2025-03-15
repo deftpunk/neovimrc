3717a269bca8baf48a0a229ccbb67f068053fb6f
Erick Bodine <erick.bodine@gmail.com>
2025-03-15 17:23:56 -0600

    Getting lualine and tabline together.

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,4 +1,7 @@
+-- vim: nu fdm=marker
+
 return {
+
   -- tokyonight colorscheme {{{
   -- https://github.com/folke/tokyonight.nvim
   {
@@ -24,10 +27,16 @@ return {
     config = function()
       require('lualine').setup {
         options = {
+          -- theme  = 'evil_lualine',
           theme  = 'tokyonight',
+          always_show_tabline = true,
         },
         sections = {
-          -- lualine_a = ...
+          lualine_a = {
+            'tabs',
+            tab_max_length = 50,
+            mode = 2,
+          },
           -- lualine_b = ...
           lualine_c = {
               { "filename", path = 1 },
@@ -39,63 +48,17 @@ return {
               },
           },
         },
+        tabline = {
+          lualine_a = {'tabs'},
+          lualine_b = {'buffers'},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {}
+        },
       }
     end,
   },
-  -- }}}
-
-  --tabby.nvim {{{
-  {
-    'nanozuki/tabby.nvim',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-      vim.o.showtabline = 2  -- always display the tabline.
-      require('tabby').setup({
-        lualine_theme = 'tokyonight',
-        nerdfront = true,
-      })
-    end,
-  },
-  -- }}}
-
-  -- tabline {{{
-  -- https://github.com/kdheepak/tabline.nvim
-  -- {
-  --   'kdheepak/tabline.nvim',
-  --   config = function()
-  --     require'tabline'.setup {
-  --       -- Defaults configuration options
-  --       enable = true,
-  --       options = {
-  --         -- If lualine is installed tabline will use separators configured in lualine by default.
-  --         -- These options can be used to override those settings.
-  --         max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-  --         show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
-  --         show_devicons = true, -- this shows devicons in buffer section
-  --         show_bufnr = false, -- this appends [bufnr] to buffer section,
-  --         show_filename_only = false, -- shows base filename only instead of relative path in filename
-  --         modified_icon = "+ ", -- change the default modified icon
-  --         modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
-  --       }
-  --     }
-  --     vim.cmd[[
-  --     set guioptions-=e " Use showtabline in gui vim
-  --     set sessionoptions+=tabpages,globals " store tabpages and globals in session
-  --     ]]
-  --   end,
-  --   dependencies = {
-  --     'nvim-lualine/lualine.nvim',
-  --     'kyazdani42/nvim-web-devicons',
-  --   },
-  -- },
-  -- }}}
-
-	-- Beacon {{{
-	-- Highlight where your cursor is when you move around.
-	-- https://github.com/DanilaMihailov/beacon.nvim
-  -- {
-  --   'DanilaMihailov/beacon.nvim'
-  -- },
   -- }}}
 
   -- vim-active-numbers {{{
